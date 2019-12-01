0d9ccd5fd9d98a10941680342f920be86e5f990e
fivosts <fivos_ts@hotmail.com>
2019-12-01 20:19:57 +0000

    Test function call for split sets
    Change-Id: I148889776c612592044d1d0c66f21a12f0e52b14

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 import dispatcher as dsp
 import architecture as arc
+import utils
 # d = dsp.dispatcher()
 # d.dispatch_projects()
 
 aleth_arc = arc.architecture()
 
+split_list = utils.generate_splitted_exclusive(60, 0.5)
+split_set = {'pass': split_list[0], 'fail_wrong_numbers': split_list[1]}
 
+print(split_set)
 
-aleth_arc.initialize_architecture(model_file = "ase_model.pymodel", project_name = "aleth", mode = "inference",
+aleth_arc.initialize_architecture(model_file = "ase_model.pymodel", project_name = "aleth", mode = "inference", split_trace_sets = split_set,
 									trace_name = "DifficultyTest", model_path = "{}_postorder/pymodel/epoch_16".format(str(int(100*0.3))), 
 									excluded_labels = ["fail_bin_ops", "fail_inverted_minus", 
 														"fail_inverted_plus", "fail_swapped_args"])
