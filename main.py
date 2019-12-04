fe304833f343359781d283764936d9c5ab16f843
fivosts <fivos_ts@hotmail.com>
2019-12-04 18:50:53 +0000

    remake dataset in main
    Change-Id: I1591c75d7010a24ebec2adf822351b57f7881266

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ import utils
 # d.dispatch_projects()
 
 aleth_arc = arc.architecture()
+remake = True
 
 for p in [0.3, 0.4, 0.5, 0.6, 0.7]:
 
 	split_list = utils.generate_splitted_exclusive(2254, p)
 	split_set = {'pass': split_list[0], 'fail_wrong_numbers': split_list[1]}
+	remake = True
 
-	print(split_set)
+	print("DBG: " + split_set)
+	
+	for tr_l in [0.05, 0.1, 0.15, 0.2, 0.3]:
 
-	SUPERVISED_TRAIN_LENGTH = [0.05, 0.1, 0.15, 0.2, 0.3]
-
-	for tr_l in SUPERVISED_TRAIN_LENGTH:
-
-		aleth_arc.initialize_architecture(model_file = "ase_model.pymodel", project_name = "aleth", split_trace_sets = split_set,
+		print("DBG: " + tr_l)
+		aleth_arc.initialize_architecture(model_file = "ase_model.pymodel", project_name = "aleth", split_trace_sets = split_set, remake_dataset = remake
 											trace_name = "DifficultyTest", save_training_log = True, save_model_specs = True, save_model = False,
 											model_path = "only_mutated/passperc{}_trl{}".format(str(int(100*p)), str(int(100*tr_l))), epochs = 40, training_length = tr_l,
 											excluded_labels = ["fail_bin_ops", "fail_inverted_minus", 
 																"fail_inverted_plus", "fail_swapped_args"])
+		remake = False
