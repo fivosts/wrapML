57798ea16c7195a72d5a639f8d9d3912b1c1be01
fivosts <fivos_ts@hotmail.com>
2020-02-05 16:56:03 +0000

    Extend mail agent to class and refactor

--- mail_agent.py
+++ mail_agent.py
@@ -1,60 +1,71 @@
 #!/usr/bin/env python
 import smtplib, ssl, imaplib
 from email.mime.text import MIMEText
+import email
+import subprocess
 
+class mail_agent:
 
-port = 465
-smtp_server = "smtp.gmail.com"
-sender_email = "ml.framew@gmail.com" 
-receiver_email = "fivos_ts@hotmail.com" 
-password = "prscustom24"
+    port = 465
+    smtp_server = "smtp.gmail.com"
+    sender_email = "ml.framew@gmail.com" 
+    receiver_email = "fivos_ts@hotmail.com" 
+    password = "prscustom24"
 
-def broadcast_error(reporting_module, error, sender = sender_email, password = "", receiver = receiver_email, request_reply = False):
+    def __init__(this):
+        return
 
-	if password == "":
-		assert False, "SMTP Server password for {} not specified!".format(sender_email)
+    def broadcast_error(this, reporting_module, error, sender = "ml.framew@gmail.com", receiver = "fivos_ts@hotmail.com", request_reply = False):
 
-	message = MIMEText("Error Reported:\n\n---------------------------------------\n{}\n---------------------------------------\n\nError reported by ML mail agent".format(error))
-	message['Subject'] = "{} crashed!".format(reporting_module)
-	message['From'] = sender
-	message['To'] = receiver
+        if this.password == "":
+            assert False, "SMTP Server password for {} not specified!".format(sender_email)
 
-	context = ssl.create_default_context()
-	with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-		server.login(sender_email, password)
-		server.sendmail(sender_email, receiver_email, message.as_string())
+        message = MIMEText("Error Reported:\n\n---------------------------------------\n{}\n---------------------------------------\n\nError reported by ML mail agent".format(error))
+        message['Subject'] = "{} crashed!".format(reporting_module)
+        message['From'] = sender
+        message['To'] = receiver
 
-	return
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(this.smtp_server, this.port, context=context) as server:
+            print(this.sender_email)
+            print(this.password)
+            server.login(this.sender_email, this.password)
+            server.sendmail(this.sender_email, this.receiver_email, message.as_string())
 
-def receive_instruction():
+        return
 
-	mail = imaplib.IMAP4_SSL('imap.gmail.com')
-	mail.login(sender_email, password)
-	mail.list()
-	# Out: list of "folders" aka labels in gmail.
-	mail.select("inbox") # connect to inbox.
+    def receive_instruction(this):
 
-	result, data = mail.search(None, "ALL")
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login(this.sender_email, this.password)
+        mail.list()
 
-	print(result)
-	print(data)
+        # Out: list of "folders" aka labels in gmail.
+        mail.select("inbox", readonly=True) # connect to inbox.
+        result, data = mail.search(None, "ALL")
 
-	ids = data[0] # data is a list.
-	print(ids)
-	id_list = ids.split() # ids is a space separated string
-	print(id_list)
+        id_list = data[0].split() # ids is a space separated string
+        latest_email_id = id_list[-1] # get the latest
 
-	latest_email_id = id_list[-1] # get the latest
+        result, data = mail.fetch(latest_email_id, "(UID BODY[TEXT])") # fetch the email body (RFC822) for the given ID
+        raw_email = data[0][1] # here's the body, which is raw text of the whole email
+        # including headers and alternate payloads
 
-	result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+        command = ""
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_bytes(response_part[1])
+                msg_list = msg.as_string().split('\n')
+                for line in msg_list:
+                	if "$cmd" in line:
+                		command = line.split(':')[1]
+                		break
 
-	raw_email = data[0][1] # here's the body, which is raw text of the whole email
-	# including headers and alternate payloads
-	print(raw_email)
+        proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        print(out.decode("utf-8"))
+        return
 
-	return
-
-body = "  File \"<stdin>\", line 1, in <module>\nFileNotFoundError: [Errno 2] No such file or directory: \'dimitroula.txt\'"
-
-broadcast_error("Cluster classifier", body, password = "prscustom24")
-# receive_instruction()
+mail = mail_agent()
+# mail.broadcast_error("Random module", "Hello from the other side !")
+mail.receive_instruction()
